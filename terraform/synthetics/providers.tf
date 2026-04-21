ddd7c04278f259da9bfc83fbbc2de3028b87c3a9
tamil danie <tamildanie@gmail.com>
2026-04-21 13:59:15 +0200

    ads

--- terraform/synthetics/providers.tf
+++ terraform/synthetics/providers.tf
@@ -1,6 +1,6 @@
 provider "grafana" {
   url             = "https://tamildanie.grafana.net/"
   auth            = var.grafana_service_token
-  sm_url          = "https://synthetic-monitoring-api-eu-north-0.grafana.net""
+  sm_url          = "https://synthetic-monitoring-api-eu-north-0.grafana.net"
   sm_access_token = var.sm_access_token
 }
