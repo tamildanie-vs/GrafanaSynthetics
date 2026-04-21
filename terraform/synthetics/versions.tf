a60ba7ce74bc86a2dad68bcc45af77c4ed008ac4
tamil danie <tamildanie@gmail.com>
2026-04-21 14:28:22 +0200

    test

--- terraform/synthetics/versions.tf
+++ terraform/synthetics/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.5.0"
 
   cloud {
     organization = "tamildanie"
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     grafana = {
       source  = "grafana/grafana"
-      version = ">= 2.0.0"
+      version = ">= 2.9.0"
     }
   }
 }
